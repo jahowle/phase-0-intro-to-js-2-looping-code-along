40fe86178459f8e46f662dc5644b2069fc5832a5
Jonny Howle <jonny.howle@gmail.com>
2022-03-29 13:13:20 -0600

    commit tree ba0a240b293a12ba010583f919840b5bf6337816

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const occasion = "surprise";
+let int = 10;
+
+function writeCards(cardNames, cardOccasion) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+
+    return messages;
+}
+
+writeCards(names, occasion);
+
+console.log(messages);
+
+function countDown (int) {
+    while (int > -1) {
+        console.log(int--);
+    }
+
+}
+
+
+
+
+
